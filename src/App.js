25fc2e92b7faf8aa1fb7b1197354a21d145353ce
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-10-04 21:01:28 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -20,14 +20,7 @@ function App() {
         <Route path='/search-project' element={<SearchProject/>}/>
         <Route path='/tech-person' element={<TechPerson/>}/>
       </Routes>
-      {/* <div>
-        <SideMenu menuName="Home"/>
-        <SideMenu menuName="Cadastro de Usuáries"/>
-        <SideMenu menuName="Cadastro de Projetos"/>
-        <SideMenu menuName="Buscar projetos"/>
-        <SideMenu menuName="Personalidades"/>
-        <SideMenu menuName="Quem somos"/>
-      </div> */}
+   
     </div>
   );
 }

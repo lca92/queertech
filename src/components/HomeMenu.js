56789c9bc2c87688f28b5c4cb84419203d8c4164
Lena Araújo <lenaaraujo92@gmail.com>
2022-10-12 19:06:44 -0300

    css - home, cadastro de profissionais, cadastro de projetos, quem somos

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -7,37 +7,37 @@ const HomeMenu = () => {
       <nav className="homeMenu">
         <ul>
           <div className="homePrincipal">
-            <Link to="/" style={{textDecoration:'none'}}>
-              <li>Home </li>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <li>for (queer in tech)</li>
             </Link>
           </div>
 
           <div className="accountPrincipal">
-            <Link to="/account" style={{textDecoration:'none'}}>
+            <Link to="/account" style={{ textDecoration: "none" }}>
               <li>Cadastro de Profissional</li>
             </Link>
           </div>
 
           <div className="newProjectPrincipal">
-            <Link to="/new-project" style={{textDecoration:'none'}}>
+            <Link to="/new-project" style={{ textDecoration: "none" }}>
               <li>Cadastro de Projeto</li>
             </Link>
           </div>
 
           <div className="searchPrincipal">
-            <Link to="/search" style={{textDecoration:'none'}}>
+            <Link to="/search" style={{ textDecoration: "none" }}>
               <li>Buscar</li>
             </Link>
           </div>
 
           <div className="techPersonPrincipal">
-            <Link to="/tech-person" style={{textDecoration:'none'}}>
+            <Link to="/tech-person" style={{ textDecoration: "none" }}>
               <li>Personalidades</li>
             </Link>
           </div>
 
           <div className="aboutPrincipal">
-            <Link to="/about" style={{textDecoration:'none'}}>
+            <Link to="/about" style={{ textDecoration: "none" }}>
               <li>Quem Somos</li>
             </Link>
           </div>
@@ -45,6 +45,6 @@ const HomeMenu = () => {
       </nav>
     </div>
   );
-}
+};
 
 export default HomeMenu;
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,25 +1,38 @@
 import React from "react";
+import "./profileCard.css";
 
-const ProfileCard = ({ profileImg, name, bio, github, linkedin }) => {
+const ProfileCard = ({ profileImg, name, flag, bio, github, linkedin }) => {
   return (
-    <div>
-      <img src={profileImg} alt="Profile" />
+    <div className="profileCard">
       <div>
-        <p className="name">{name}</p>
-        <p className="bio">{bio}</p>
-
-          <a href={github} className="github">
-            <img src="" alt="Github Icon" />
-            <p>Github</p>
-          </a>
-
-          <a href={linkedin} className="linkedin">
-            <img src="" alt="LinkedIn Icon" />
-            <p>LinkedIn</p>
-          </a>
-
+        <div className="cropPic">
+          <img src={profileImg} alt="Profile" />
         </div>
       </div>
+
+      <div>
+        <div>
+          <div className="person">
+            <p className="name">{name}</p>
+            <img className="flag" src={flag} alt="Flag" />
+          </div>
+
+          <div className="bio">
+            <p>{bio}</p>
+          </div>
+
+          <div className="professionalProfiles">
+            <a href={github} className="github">
+              <p>Github</p>
+            </a>
+
+            <a href={linkedin} className="linkedin">
+              <p>LinkedIn</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 

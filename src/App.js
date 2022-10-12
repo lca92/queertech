8789d1b0e25e01fb3db59dcc271bc01d6ba18a8b
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-10-11 23:26:29 -0300

    created project form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./components/pages/Home.js";
 import About from "./components/pages/About.js";
 import Account from "./components/pages/Account.js";
-import NewProject from "./components/pages/NewProject.js";
+import Project from "./components/pages/Project.js";
 import Search from "./components/pages/Search.js";
 import TechPerson from "./components/pages/TechPerson.js";
 
@@ -18,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/account" element={<Account />} />
-          <Route path="/new-project" element={<NewProject />} />
+          <Route path="/new-project" element={<Project />} />
           <Route path="/search" element={<Search/>} />
           <Route path="/tech-person" element={<TechPerson />} />
         </Routes>
